6c168ba2fbb0f25a03e4b059307df12d5ac1a926
Pranav Kumar Varshney <89956447+pranav2310@users.noreply.github.com>
2024-07-18 18:32:30 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
     useEffect(() => {
         setLoading(true);
         axios
-            .get('http://localhost:5555/books')
+            .get('http://book-store-mern-api-kappa.vercel.app')
             .then((response) => {
                 setBooks(response.data.data);
                 setLoading(false);
